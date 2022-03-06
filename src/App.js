43d2ddc9e4eea6919c6179af3a85eb06ffd05bfa
Anderson Bautista <anderbarker86@gmail.com>
2022-03-06 12:10:10 -0500

    about page first sketch done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navabar';
 import Routes from './routes'
 import './App.css';
 import './sass/main.scss'
+import Footer from './components/footer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+    <div className="footer-social">
+        <a className="footer-links" href="https://www.linkedin.com/in/andersonbb/" target="_blank">LinkedIn</a>
+        <a className="footer-links" href="https://github.com/AndersBau" target="_blank">github</a>
+        <a className="footer-links" href="https://dribbble.com/Andev11" target="_blank">Dribbble</a>
+    </div>
+    <p>© 2022 Anderson Bautista</p>
+</footer>
+  )
+}
+
+export default Footer;

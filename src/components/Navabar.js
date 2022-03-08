9b050d7584620d47e04abb1cd5962491ec8da47b
Anderson Bautista <anderbarker86@gmail.com>
2022-03-08 09:49:06 -0500

    navbar almost done

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] =useState(false);
+
+  function openMenu() {
+    setMenuIsOpen(!menuIsOpen);
+  };
+useEffect(()=> {
+
+}, [openMenu])
+
+
   return (
     <header id="header">
       <div className="fixedmenu"></div>
       <nav className="nav">
-        <ul className="nav_ul">
-          <li className="nav_li"><Link to="/">Home</Link></li>
-          <li className="nav_li"><Link to="/about">About</Link></li>
-          <li className="nav_li"><Link to="/portfolio">Portfolio</Link></li>
-          <li className="nav_li"><Link to="/contact">Contact</Link>    </li>
+        <ul className={menuIsOpen ? "nav_ul nav_ul--open" : "nav_ul"}>
+          <li className="nav_li"><Link to="/" className="nav_li--link" onClick={() => openMenu()}>Home</Link></li>
+          <li className="nav_li"><Link to="/about" className="nav_li--link" onClick={() => openMenu()}>About</Link></li>
+          <li className="nav_li"><Link to="/portfolio" className="nav_li--link" onClick={() => openMenu()}>Portfolio</Link></li>
+          <li className="nav_li"><Link to="/contact" className="nav_li--link" onClick={() => openMenu()}>Contact</Link>    </li>
         </ul>
 
-        <div className="nav_burguer">
-          <div className="nav_burguer--1"></div>
-          <div className="nav_burguer--2"></div>
-          <div className="nav_burguer--3"></div>
+        <div className="nav_burguer" onClick={() => openMenu()}>
+          <div className={ menuIsOpen ? "nav_burguer--1 nav_b1--active" : "nav_burguer--1"}></div>
+          <div className={ menuIsOpen ? "nav_burguer--2 nav_b2--active" : "nav_burguer--2"}></div>
+          <div className={ menuIsOpen ? "nav_burguer--3 nav_b3--active" : "nav_burguer--3"}></div>
         </div>
       </nav>
       <Link to="/">
